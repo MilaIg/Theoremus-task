e0e0c8649c67349c1b7765304b374d79d06fca5d
MilaIg <mila_ignatova@hotmail.com>
2023-09-09 14:50:48 +0000

    Second commit

--- ingress/create-new-cluster.sh
+++ ingress/create-new-cluster.sh
@@ -1,3 +1,4 @@
 ./configure/ingress-nginx/install.sh
 ./configure/ingress-nginx/install.sh
 kubectl create secret tls sourcegraph-tls --key .envrc/private.key --cert .envrc/public.pem
+./configure/ingress-nginx/install.sh
